2014b7ba845521dcf9583419fd9ad57b85e21a26
tsurik <tomas.surik@gmail.com>
2015-08-20 19:33:52 +0200

    commit 3

--- run_analysis.r
+++ run_analysis.r
@@ -1,8 +1,9 @@
 #LOADING DATA
-test<-read.table("./UCI HAR Dataset/test/X_test.txt", quote="\"", comment.char="")
+
 train<-read.table("./UCI HAR Dataset/train/X_train.txt", quote="\"", comment.char="")
+test<-read.table("./UCI HAR Dataset/test/X_test.txt", quote="\"", comment.char="")
 #MERGING DATA
-fulldata<-merge(test,train,all=TRUE)
+fulldata<-merge(train,test,all=TRUE)
 #MEAN AND SD EXTRACTION
 features<-read.table("./UCI HAR Dataset/features.txt", quote="\"", comment.char="")
 names(features)<-c("id","featurename")
@@ -10,3 +11,8 @@ means<-grep(".mean.",features$featurename)
 stds<-grep(".std.",features$featurename)
 index<-c(means,stds)
 subsdata<-fulldata[,sort(index)]
+names(subsdata)<-features$featurename[sort(index)]
+#ACTIVITIES
+trainactivities<- read.table("C:/RWD/UCI HAR Dataset/train/y_train.txt", quote="\"", comment.char="")
+testactivities <- read.table("C:/RWD/UCI HAR Dataset/test/y_test.txt", quote="\"", comment.char="")
+activities<c(as.vector(trainactivities$V1),as.vector(stactivities$V1))
